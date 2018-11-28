74a532de32605ea3baf3abc602ddb09ec41cd831
Ibraheem Moosa <ibraheemmoosa1@gmail.com>
2018-11-28 11:53:26 -0600

    Update multiplication.cpp

--- multiplication.cpp
+++ multiplication.cpp
@@ -2,8 +2,8 @@
 #include <iostream>
 using namespace std;
 
-complexValue operator * (complexValue& lhs, complexValue* rhs){
-    complexValue temp = *rhs;
+complex operator * (complex& lhs, complex* rhs){
+    complex temp = *rhs;
     temp.real = (lhs.real * rhs->real) - (lhs.im * rhs->im);
     temp.im = (lhs.real * rhs->im) + (lhs.im * rhs->real);
     return temp;
