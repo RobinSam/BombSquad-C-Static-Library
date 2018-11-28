f6ce6e0a1da84e1baf156cd09cb46a93687d8cf8
C1MM <40035427+C1MM@users.noreply.github.com>
2018-11-28 12:00:43 -0600

    Created minus

---
+++ minus.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "COMPLEXCLASS.h"
+using namespace std:
+complexValue operator -(complexValue& lhs, complexValue* rhs) {
+	ComplexValue temp = new ComplexValue();
+	temp.real=arg1.real+arg2->real;
+	return temp;
+}
