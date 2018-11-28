4af0b8366bbd9d46dbf774336e1ffcd3c4a2556b
C1MM <40035427+C1MM@users.noreply.github.com>
2018-11-28 11:24:04 -0600

    Revert "Created minus function"
    This reverts commit a657295b3c070e7971f176fcfe599cfce9f351ea.

--- minus.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include COMPLEXCLASS.h
-using namespace std:
-complexValue operator -(complexValue arg1, complexValue arg2) {
-	ComplexValue temp = new ComplexValue();
-	temp.real=arg1->real+arg2->real;
-	return temp;
-}
