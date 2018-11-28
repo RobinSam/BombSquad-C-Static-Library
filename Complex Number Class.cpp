d296f3a5ad42d7d9a5539af7d8b28c6b10afd251
lucasteltow <45432531+lucasteltow@users.noreply.github.com>
2018-11-28 10:54:48 -0600

    Create Complex Number Class.cpp

---
+++ Complex Number Class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class complexValue
+{
+    double real, im;
+    public:
+        complexValue(double r, double i)
+           { 
+               real = r;
+               im = i;
+           } 
+};
