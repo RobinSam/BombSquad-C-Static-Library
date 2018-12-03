cebb908f8202db8175f61803fe79549c3670764d
RobinSam <robin787.rs@gmail.com>
2018-12-02 20:28:22 -0600

    updated all operator prototypes

--- COMPLEXCLASS.h
+++ COMPLEXCLASS.h
@@ -5,14 +5,14 @@ using namespace std;
 class complex
 {
     double re, im;
-    friend complex operator +(complex& lhs, complex* rhs);
-    friend complex operator -(complex& lhs, complex* rhs);
-    friend complex operator *(complex& lhs, complex* rhs);
-    friend complex operator /(complex& lhs, complex* rhs);
-    friend complex operator +=(complex& lhs, complex* rhs);
-    friend complex operator -=(complex& lhs, complex* rhs);
-    friend complex operator *=(complex& lhs, complex* rhs);
-    friend complex operator /=(complex& lhs, complex* rhs);
+    friend complex operator +(complex& lhs, complex& rhs);
+    friend complex operator -(complex& lhs, complex& rhs);
+    friend complex operator *(complex& lhs, complex& rhs);
+    friend complex operator /(complex& lhs, complex& rhs);
+    friend complex operator +=(complex& lhs, complex& rhs);
+    friend complex operator -=(complex& lhs, complex& rhs);
+    friend complex operator *=(complex& lhs, complex& rhs);
+    friend complex operator /=(complex& lhs, complex& rhs);
     public:
         complex();
         complex(double r, double i);
