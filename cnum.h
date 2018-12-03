73ed0a5810b4f9448078e44b24efba89cb19c70e
RobinSam <robin787.rs@gmail.com>
2018-12-02 20:55:55 -0600

    Changed "complex" to "cnum"

---
+++ cnum.h
@@ -0,0 +1,26 @@
+#ifndef CNUM_H
+#define CNUM_H
+#include "add.h"
+#include "multiplication.h"
+using namespace std;
+
+class cnum
+{
+    double re, im;
+    friend cnum operator +(cnum& lhs, cnum& rhs);
+    friend cnum operator -(cnum& lhs, cnum& rhs);
+    friend cnum operator *(cnum& lhs, cnum& rhs);
+    friend cnum operator /(cnum& lhs, cnum& rhs);
+    friend cnum operator +=(cnum& lhs, cnum& rhs);
+    friend cnum operator -=(cnum& lhs, cnum& rhs);
+    friend cnum operator *=(cnum& lhs, cnum& rhs);
+    friend cnum operator /=(cnum& lhs, cnum& rhs);
+    public:
+        cnum();
+        cnum(double r, double i);
+        getReValue();
+        getImValue();
+        getValue();
+};
+#endif
+
