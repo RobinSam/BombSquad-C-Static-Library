874d3ff23b0d02c4014f88afcdbccd49f869e723
C1MM <40035427+C1MM@users.noreply.github.com>
2018-12-03 10:09:17 -0600

    Created minusequals

---
+++ minusequals.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "COMPLEXCLASS.h"
+void operator -(complexValue& lhs, complexValue* rhs) {
+	ComplexValue temp = new ComplexValue();
+	temp.real=arg1.real+arg2->real;
+	lhs = temp;
+}
