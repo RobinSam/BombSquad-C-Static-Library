cb53845e8b5ce1305f41e33c5865c572115e8515
RobinSam <robin787.rs@gmail.com>
2018-12-03 22:17:52 -0600

    Finished division

--- division.h
+++ division.h
@@ -6,6 +6,7 @@ using namespace std;
 
 Complex operator /(Complex& lhs, Complex& rhs){
    Complex temp = rhs;
-   
-
+   temp.re = ((lhs.re*rhs.re)+(lhs.im*rhs.im))/(rhs.re*rhs.re+rhs.im*rhs.im);
+   temp.im = (lhs.im*rhs.re-lhs.re*rhs.im)/(rhs.re*rhs.re+rhs.im*rhs.im);
+   return temp;
 }
