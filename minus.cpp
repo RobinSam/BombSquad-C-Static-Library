106d8cfd046d57912ecfe33b4cda6105d148a3b8
RobinSam <robin787.rs@gmail.com>
2018-12-05 12:26:34 -0600

    Update and rename minus.h to minus.cpp

--- minus.cpp
+++ minus.cpp
@@ -1,12 +1,9 @@
-#ifndef MINUS_H
-#define MINU_H
 #include "complexnum.h"
 using namespace std;
 
-Complex operator -(Complex& lhs, Complex& rhs) {
+Complex Complex:: operator -(Complex& lhs, Complex& rhs) {
 	Complex temp = Complex();
 	temp.re=lhs.re-rhs.re;
 	temp.im=lhs.im-rhs.im;
 	return temp;
 }
-#endif
