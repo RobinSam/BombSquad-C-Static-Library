a77efab914216c748039fb4343e78b124e522155
RobinSam <robin787.rs@gmail.com>
2018-12-05 12:24:53 -0600

    Update division.h

--- division.h
+++ division.h
@@ -1,10 +1,7 @@
-#ifndef DIVISION_H
-#define DIVISION_H
-#include "complexnum.h"
-
+#include "complex.h"
 using namespace std;
 
-Complex operator /(Complex& lhs, Complex& rhs){
+Complex Complex:: operator /(Complex& lhs, Complex& rhs){
    Complex temp = rhs;
    temp.re = ((lhs.re*rhs.re)+(lhs.im*rhs.im))/(rhs.re*rhs.re+rhs.im*rhs.im);
    temp.im = (lhs.im*rhs.re-lhs.re*rhs.im)/(rhs.re*rhs.re+rhs.im*rhs.im);
