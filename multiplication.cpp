a04c00054848a7a7e34869f8b9a669f365ae52d6
RobinSam <robin787.rs@gmail.com>
2018-12-05 12:27:49 -0600

    Update and rename multiplication.h to multiplication.cpp

--- multiplication.cpp
+++ multiplication.cpp
@@ -1,11 +1,8 @@
-#ifndef MULTIPLICATION_H
-#define MULTIPLICATION_H
-#include "complexnum.h"
+#include "complex.h"
 
-Complex operator *(Complex& lhs, Complex& rhs){
+Complex Complex:: operator *(Complex& lhs, Complex& rhs){
     Complex temp = rhs;
     temp.re = (lhs.re * rhs.re) - (lhs.im * rhs.im);
     temp.im = (lhs.re * rhs.im) + (lhs.im * rhs.re);
     return temp;
 }
-#endif
