3a88d25df53215fad68491c4fd772ab50752f822
C1MM <40035427+C1MM@users.noreply.github.com>
2018-12-07 11:19:57 -0600

    Create cin.cpp

---
+++ cin.cpp
@@ -0,0 +1,8 @@
+#include <math.h>
+#include "complex.h"
+void operator >>(istream& lhs, Complex& rhs) {
+string input1 = "";
+istream>>input1;
+rhs.re=stod(input1.substr(0,3));
+rhs.im = stod(input1.substr(4,3));
+}
