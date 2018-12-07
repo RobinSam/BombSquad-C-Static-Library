babffc2c6beda66da83df6e5a4cfcfb5a7adeb49
RobinSam <robin787.rs@gmail.com>
2018-12-07 10:28:27 -0600

    Update minus.cpp

--- minus.cpp
+++ minus.cpp
@@ -1,9 +1,9 @@
 #include "complex.h"
 using namespace std;
 
-Complex Complex:: operator -(Complex& lhs, Complex& rhs) {
-	Complex temp = Complex();
-	temp.re=lhs.re-rhs.re;
-	temp.im=lhs.im-rhs.im;
+Complex Complex:: operator -(Complex& rhs) {
+	Complex temp = *this;
+	temp.re=temp.re-rhs.re;
+	temp.im=temp.im-rhs.im;
 	return temp;
 }
