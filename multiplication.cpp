4da489ec62583c6de4d26ce669c6ddbc4f57a765
Ibraheem Moosa <ibraheemmoosa1@gmail.com>
2018-12-07 10:24:42 -0600

    change lhs to this

--- multiplication.cpp
+++ multiplication.cpp
@@ -1,8 +1,8 @@
 #include "complex.h"
 
-Complex Complex:: operator *(Complex& lhs, Complex& rhs){
-    Complex temp = rhs;
-    temp.re = (lhs.re * rhs.re) - (lhs.im * rhs.im);
-    temp.im = (lhs.re * rhs.im) + (lhs.im * rhs.re);
+Complex Complex:: operator *(Complex& rhs){
+    Complex temp = *this;
+    temp.re = (temp.re * rhs.re) - (temp.im * rhs.im);
+    temp.im = (temp.re * rhs.im) + (temp.im * rhs.re);
     return temp;
 }
