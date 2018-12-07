98f4840b56318fa7440dbb18a209269a8a35aeca
RobinSam <robin787.rs@gmail.com>
2018-12-07 11:05:34 -0600

    Update timesequals.cpp

--- timesequals.cpp
+++ timesequals.cpp
@@ -1,7 +1,7 @@
 #include "complex.h"
 
 void Complex:: operator *=(Complex& rhs) {
-   Complex temp = *this;
-	temp=temp*rhs;
-	this = &temp;
+	Complex temp = *this;
+   	temp=temp*rhs;
+	*this = temp;
 }
