72b54edd0ab356708f85a21d3cb75d7e928611db
lucasteltow <45432531+lucasteltow@users.noreply.github.com>
2018-12-10 11:12:39 -0600

    Added Comments

--- equals.cpp
+++ equals.cpp
@@ -1,5 +1,14 @@
 #include "complex.h"
 
+/* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Title: Complex Equals Operator
+Author: Lucas Teltow 
+Description: Overloads = operator to allow for setting one complex equal to another. 
+Created: 12/4/2018
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
+
 void Complex:: operator =(const Complex& rhs)
 {
    Complex temp = *this;
--- timesequals.cpp
+++ timesequals.cpp
@@ -1,14 +1,5 @@
 #include "complex.h"
 
-/* 
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-Title: Complex Multiply Equals Operator
-Author: Lucas Teltow 
-Description: Overloads *= operator to allow for multiplication of complex number times itself and another number. 
-Created: 12/4/2018
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-*/
-
 void Complex:: operator *=(const Complex& rhs) {
 	Complex temp = *this;
    temp=temp*rhs;
