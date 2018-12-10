25b9eb17729cc18f1f4c6f4142b18470d35d7190
RobinSam <robin787.rs@gmail.com>
2018-12-10 11:54:39 -0600

    Update arg.cpp

--- arg.cpp
+++ arg.cpp
@@ -2,18 +2,25 @@
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 Title: Complex Arg Function
 Author: Robin Sam 
-Description: Calculates the phase of a complex number in Radians.
+Description: Function for the phase of a complex number in Radians.
 Created: 12/10/2018
 Modified: 12/10/2018
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 */
+/*
+ * Summary: Calculates the degrees in Radians for a complex number after checking to see
+ *          if the real, imaginary or both are negative and subtracting from PI if needed.
+ * Parameters:   complex rhs - the complex number for which the angle is being calculated.
+ * Return:       degrees in Radians of type double
+ */
 
 #include "math.h"
 #include "complex.h"
 #define PI 3.14159265
 
 double arg(Complex& rhs) {
-if (rhs.re<0 && rhs.im<0)
+//check if re is <0, im is <0 or both are <0
+if (rhs.re<0 && rhs.im<0) 
   return atan(rhs.im/rhs.re)+PI;
 else if (rhs.re < 0 && rhs.im >0)
   return PI-atan(rhs.im/rhs.re);
