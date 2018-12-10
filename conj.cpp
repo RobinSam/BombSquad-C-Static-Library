44ecfc500a8cea9ea6a219a20257f43d1b1152b4
RobinSam <robin787.rs@gmail.com>
2018-12-10 11:57:38 -0600

    Update conj.cpp

--- conj.cpp
+++ conj.cpp
@@ -1,3 +1,16 @@
+/* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Title : Complex conjugate function
+Author : Chinmay Murthy
+Description :Returns the conjugate of the input
+Created : 12/5/2018
+Modified : 12/5/2018
+*/
+/*
+ * Summary: Takes a complex number and multiplies the imaginary part by -1
+ * Parameters: Complex number to take conjugate of
+ * Return:  Complex number with a negative imaginary part
+ */
 #include "complex.h"
 
 using namespace std;
