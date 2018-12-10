8aea121bd2304f38ee8c487cad7a3065bf079224
Ibraheem Moosa <ibraheemmoosa1@gmail.com>
2018-12-10 11:08:10 -0600

    Update multiplication.cpp

--- multiplication.cpp
+++ multiplication.cpp
@@ -1,5 +1,14 @@
 #include "complex.h"
 
+ /* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Title : Multiplication Function
+Author : Ibraheem Moosa
+Description : Overloads * operator to allow for multiplication of complex numbers.
+Created : 12/2/18
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
+
 Complex Complex:: operator *(const Complex& rhs){
     Complex temp = *this;
     temp.re = (temp.re * rhs.re) - (temp.im * rhs.im);
