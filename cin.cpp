b6f45ba41b966bd12594ff70e6ea7523fcce9d2b
RobinSam <robin787.rs@gmail.com>
2018-12-10 11:40:42 -0600

    Update cin.cpp

--- cin.cpp
+++ cin.cpp
@@ -4,25 +4,18 @@ Title: Complex Arg Function
 Author: Robin Sam 
 Description: Overloads >> operator to accurately take in the parameters of a complex number as input
              and assign the input to a predefined complex variable.
-Created: 12/10/2018
+Created: 12/7/2018
 Modified: 12/10/2018
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-*/
-
-#include <iostream>
-#include "complex.h"
-/* 
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-Title : Cin
-Author : Chinmay Murthy
-Description : Overloads cin operator to take complex numbers
-Modified : 12/7/2018
 */
 /*
  * Summary: Takes in a real and imaginary part of a complex numbers and assigns those values to a Complex object
  * Parameters:   input stream to assign data to, complex number to which the parts will be attatched
  * Return:       Input stream containing the input
  */
+#include <iostream>
+#include "complex.h"
+
 istream& operator >>(istream& lhs, Complex& rhs) {
    cout << "Enter Real Part: ";
    lhs >> rhs.re;
