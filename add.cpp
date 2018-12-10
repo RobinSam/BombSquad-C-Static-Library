6d4d672469928b90662c834a88d3240bea69b70f
Chris5407 <christopher.smith4202@gmail.com>
2018-12-10 11:24:30 -0600

    Added class + function comments

--- add.cpp
+++ add.cpp
@@ -1,5 +1,20 @@
 #include "complex.h"
 
+/* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Title: Complex Addition Operator
+Author: Chris Smith
+Description: Overloads + operator to allow for addition of complex numbers. 
+Created: 11/28/2018
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
+
+/*
+ * Summary:      Adds two complex numbers together
+ * Parameters:   rhs - The Complex number object to add with on the 
+ *               right hand side of the + operation
+ * Return:       The resulting complex number object after being added
+ */
 Complex Complex:: operator +(const Complex& rhs)
 {
     Complex temp = *this;
