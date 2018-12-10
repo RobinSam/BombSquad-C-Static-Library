81b3a7cfa9e6c757ff64b4a46ed514b2001110f8
Ibraheem Moosa <ibraheemmoosa1@gmail.com>
2018-12-10 11:26:15 -0600

    Update cout.cpp

--- cout.cpp
+++ cout.cpp
@@ -3,6 +3,22 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+
+ /* 
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+Title : Cout Function
+Author : Ibraheem Moosa
+Description : Overloads << operator to allow for output (printing) of complex numbers.
+Created : 12/2/18
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+*/
+
+/*
+ * Summary: "Couts" (displays/prints) a complex number
+ * Parameters: rhs the complex number on the right cout (print)
+ * Return: An output stream containing the complex number to be printed
+ */
+
 using namespace std;
 
 ostream& operator <<(ostream& lhs, const Complex& rhs){
