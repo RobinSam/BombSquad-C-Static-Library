89e8708c2e7052c894429ccc280eeea55eab46d1
Robin Sam <robin.sampd@stu.austinisd.org>
2018-12-10 11:07:06 -0600

    Last update to actual code

--- ComplexRunner.cpp
+++ ComplexRunner.cpp
@@ -23,15 +23,22 @@ int main ()
      cout << "a + b = " << a + b << "\n";
      cout << "a * b = " << a * b << "\n";
      cout << "a / b = " << a / b << "\n";
+     
+     cout << "Function tests" << endl;
      cout << "|a| = "   << abs(a) << "\n";
      cout << "complex conjugate of a = " << conj(a) << "\n";
      cout << "norm of a = " << norm(a) << "\n";
-     cout << "abs of a = " << abs(a) << "\n";
-     //cout << "exp(a) = " << exp(a) << "\n";
+     cout << "the sin of a = " << sin(a) << endl;
+     cout << "the phase in Radians of a = " << arg(a) << endl;
+     cout << "the sqrt of a = " << "NaN" << endl;
+     cout << "the natural log of a = " << "NaN" << endl;
+     cout << "the acosh of a = " << "NaN" << endl;
+     cout << "rounded to 2 decimal places = " << "NaN" << endl;
+     
 
    
    
-   
+   /*
     Complex z1 = Complex(2.0, 4.0);
     Complex z2 = Complex(3.0, 7.0);
    
@@ -40,14 +47,14 @@ int main ()
     cout << "Complex number example: " << z1 << endl;
     cout << "The conjugate is: " << conj(z1) << endl;
     cout << "The magnitude is: " << abs(z1) << endl;
-    cout << "The phase in Radians is: " << "NaN" << endl;
+    cout << "The phase in Radians is: " << arg(z1) << endl;
     cout << "The norm is: " << norm(z1) << endl;
     cout << "The sqrt is: " << "NaN" << endl;
     cout << "The natural log is: " << "NaN" << endl;
     cout << "The sin is: " << sin(z1) << endl;
     cout << "The acosh is: " << "NaN" << endl;
     cout << "Rounded to 2 decimal places: " << "NaN" << endl;
-     
+     */
    
 }
 
